2bf629433374145f3fe38f743d8030c44896dd56
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-10-31 20:29:38 +0200

    Allow removing of openstack migrations [WAL-7520]

---
+++ src/openstack/openstack-tenant/DeleteMigrationAction.tsx
@@ -0,0 +1,34 @@
+import { Trash } from '@phosphor-icons/react';
+import { useDispatch } from 'react-redux';
+
+import { translate } from '@waldur/i18n';
+import { waitForConfirmation } from '@waldur/modal/actions';
+import { RowActionButton } from '@waldur/table/ActionButton';
+
+import { deleteMigration } from '../api';
+
+export const DeleteMigrationAction = ({ resource, refetch }) => {
+  const dispatch = useDispatch();
+  const openDialog = async () => {
+    try {
+      await waitForConfirmation(
+        dispatch,
+        translate('Confirmation'),
+        translate('Are you sure you want to delete the migration?'),
+      );
+    } catch {
+      return;
+    }
+    await deleteMigration(resource.uuid);
+    await refetch();
+  };
+  return (
+    <RowActionButton
+      title={translate('Remove')}
+      action={openDialog}
+      variant="outline-danger"
+      iconNode={<Trash />}
+      size="sm"
+    />
+  );
+};
