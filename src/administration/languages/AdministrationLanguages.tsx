ef3a0c2f88dc396e3461ed4e725e7d3d6b7621fe
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-11-08 16:03:38 +0200

    Cover AdministrationLanguages with unit tests

--- src/administration/languages/AdministrationLanguages.tsx
+++ src/administration/languages/AdministrationLanguages.tsx
@@ -1,13 +1,76 @@
-import { FunctionComponent } from 'react';
-import { Card } from 'react-bootstrap';
+import { FunctionComponent, useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
 
 import { ENV } from '@waldur/configs/default';
+import { AwesomeCheckboxField } from '@waldur/form/AwesomeCheckboxField';
 import { translate } from '@waldur/i18n';
+import { useLanguageSelector } from '@waldur/i18n/useLanguageSelector';
+import { CountryFlag } from '@waldur/marketplace/common/CountryFlag';
+import { LanguageCountry } from '@waldur/navigation/header/LanguageSelectorDropdown';
+import { useNotify } from '@waldur/store/hooks';
 
-import LanguageOptionsForm from './LanguageOptionsForm';
+import { saveConfig } from '../settings/api';
 
 export const AdministrationLanguages: FunctionComponent = () => {
-  const languageChoices = ENV.languageChoices;
+  const [selectedLanguages, setSelectedLanguages] = useState<string[]>(
+    ENV.plugins.WALDUR_CORE.LANGUAGE_CHOICES,
+  );
+  const { showError, showSuccess } = useNotify();
+  const { currentLanguage } = useLanguageSelector();
+
+  const handleLanguageChange = (code: string) => {
+    setSelectedLanguages((prevSelectedLanguages: string | string[]) => {
+      let selectedLanguagesArray = Array.isArray(prevSelectedLanguages)
+        ? prevSelectedLanguages
+        : prevSelectedLanguages.split(',');
+
+      selectedLanguagesArray = selectedLanguagesArray.filter(
+        (lang) => lang !== '',
+      );
+      const isCodeSelected = selectedLanguagesArray.includes(code);
+
+      if (currentLanguage.code === code) {
+        showError(translate('You cannot unselect the current UI language'));
+        return selectedLanguagesArray;
+      }
+
+      if (ENV.defaultLanguage === code && isCodeSelected) {
+        showError(translate('You cannot unselect the default language'));
+        return selectedLanguagesArray;
+      }
+
+      if (isCodeSelected) {
+        return selectedLanguagesArray.filter((lang) => lang !== code);
+      } else {
+        return [...selectedLanguagesArray, code];
+      }
+    });
+  };
+
+  const saveLanguageOptions = async () => {
+    if (selectedLanguages.length === 0) {
+      showError(
+        translate('Please select at least one language to save changes'),
+      );
+    } else {
+      try {
+        const selectedLanguageCodes = selectedLanguages.join(',');
+        await saveConfig({
+          LANGUAGE_CHOICES: selectedLanguageCodes,
+        });
+        showSuccess(
+          translate(
+            'A list of languages available for selection has been updated',
+          ),
+        );
+        location.reload();
+      } catch (e) {
+        showError(
+          translate('Unable to update languages available for selection'),
+        );
+      }
+    }
+  };
 
   return (
     <Card>
@@ -16,7 +79,43 @@ export const AdministrationLanguages: FunctionComponent = () => {
       </Card.Header>
 
       <Card.Body>
-        <LanguageOptionsForm languageChoices={languageChoices} />
+        <div>
+          <div className="row" style={{ columnCount: 2, display: 'flow' }}>
+            <div className="col-md-6">
+              {ENV.languageChoices.map(
+                (language: { code: string; label: string }) => (
+                  <div key={language.code} className="mb-3">
+                    <AwesomeCheckboxField
+                      data-testid={`language_${language.code}`}
+                      name={`language_${language.code}`}
+                      input={
+                        {
+                          value: selectedLanguages.includes(language.code),
+                          onChange: () => handleLanguageChange(language.code),
+                        } as any
+                      }
+                      label={
+                        <div className="d-flex align-items-center">
+                          <div className="symbol symbol-20px me-2">
+                            <CountryFlag
+                              countryCode={LanguageCountry[language.code]}
+                            />
+                          </div>
+                          {language.label}
+                        </div>
+                      }
+                    />
+                  </div>
+                ),
+              )}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 text-end">
+              <Button onClick={saveLanguageOptions}>{translate('Save')}</Button>
+            </div>
+          </div>
+        </div>
       </Card.Body>
     </Card>
   );
