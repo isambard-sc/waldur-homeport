2c04b7f7ec2677e5ba29c6a34c8be143d76bb1cf
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-11-12 16:58:28 +0200

    Migrate test for ShowReportButton to RTL

--- src/marketplace/resources/report/ShowReportButton.tsx
+++ src/marketplace/resources/report/ShowReportButton.tsx
@@ -1,11 +1,10 @@
 import { BookOpen } from '@phosphor-icons/react';
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import { lazyComponent } from '@waldur/core/lazyComponent';
 import { translate } from '@waldur/i18n';
 import { Report } from '@waldur/marketplace/resources/types';
-import { openModalDialog } from '@waldur/modal/actions';
+import { useModal } from '@waldur/modal/hooks';
 
 const ShowReportDialog = lazyComponent(
   () => import('./ShowReportDialog'),
@@ -16,22 +15,20 @@ interface ShowReportButtonProps {
   report: Report;
 }
 
-const openReport = (report) =>
-  openModalDialog(ShowReportDialog, { resolve: { report }, size: 'lg' });
-
 export const ShowReportButton: React.FC<ShowReportButtonProps> = ({
   report,
 }) => {
-  const dispatch = useDispatch();
-  const callback = React.useCallback(
-    () => dispatch(openReport(report)),
-    [report, dispatch],
-  );
+  const { openDialog } = useModal();
   if (!report) {
     return null;
   }
   return (
-    <button className="btn btn-info pull-right btn-sm ms-2" onClick={callback}>
+    <button
+      className="btn btn-info pull-right btn-sm ms-2"
+      onClick={() =>
+        openDialog(ShowReportDialog, { resolve: { report }, size: 'lg' })
+      }
+    >
       <span className="svg-icon svg-icon-2">
         <BookOpen />
       </span>{' '}
