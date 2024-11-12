3347ce03a6fdb71088a2e65447d90599d1d43faf
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2024-11-12 15:26:07 +0000

    [WAL-7515] Add mass unlink and set erred action to resources

--- src/marketplace/resources/mass-actions/ResourceMultiSelectAction.tsx
+++ src/marketplace/resources/mass-actions/ResourceMultiSelectAction.tsx
@@ -1,6 +1,8 @@
 import { DropdownButton, DropdownDivider } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 import { translate } from '@waldur/i18n';
+import { getUser } from '@waldur/workspace/selectors';
 
 import { Resource } from '../types';
 
@@ -8,8 +10,10 @@ import { MultiDestroyAction } from './MultiDestroyAction';
 import { MultiMoveAction } from './MultiMoveAction';
 import { MultiPullAction } from './MultiPullAction';
 import { MultiRestartAction } from './MultiRestartAction';
+import { MultiSetErredAction } from './MultiSetErredAction';
 import { MultiStartAction } from './MultiStartAction';
 import { MultiStopAction } from './MultiStopAction';
+import { MultiUnlinkAction } from './MultiUnlinkAction';
 
 export const ResourceMultiSelectAction = ({
   rows,
@@ -17,14 +21,24 @@ export const ResourceMultiSelectAction = ({
 }: {
   rows: Resource[];
   refetch(): void;
-}) => (
-  <DropdownButton variant="primary" title={translate('All actions')}>
-    <MultiStopAction rows={rows} refetch={refetch} />
-    <MultiStartAction rows={rows} refetch={refetch} />
-    <MultiRestartAction rows={rows} refetch={refetch} />
-    <MultiPullAction rows={rows} refetch={refetch} />
-    <MultiMoveAction rows={rows} refetch={refetch} />
-    <DropdownDivider className="border-top m-0" />
-    <MultiDestroyAction rows={rows} refetch={refetch} />
-  </DropdownButton>
-);
+}) => {
+  const user = useSelector(getUser);
+  return (
+    <DropdownButton variant="primary" title={translate('All actions')}>
+      <MultiStopAction rows={rows} refetch={refetch} />
+      <MultiStartAction rows={rows} refetch={refetch} />
+      <MultiRestartAction rows={rows} refetch={refetch} />
+      <MultiPullAction rows={rows} refetch={refetch} />
+      <MultiMoveAction rows={rows} refetch={refetch} />
+      <DropdownDivider className="border-top m-0" />
+      <MultiDestroyAction rows={rows} refetch={refetch} />
+      {user.is_staff && (
+        <>
+          <DropdownDivider className="border-top m-0" />
+          <MultiSetErredAction rows={rows} refetch={refetch} />
+          <MultiUnlinkAction rows={rows} refetch={refetch} />
+        </>
+      )}
+    </DropdownButton>
+  );
+};
