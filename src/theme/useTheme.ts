48ad8993cc9864a0a5879cae4c8203c0fcb1226d
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-11-15 02:00:59 +0200

    Refactor hooks for theme

--- src/theme/useTheme.ts
+++ src/theme/useTheme.ts
@@ -2,10 +2,4 @@ import { useContext } from 'react';
 
 import { ThemeContext } from './types';
 
-export const useTheme = () => {
-  const context = useContext(ThemeContext);
-  if (context === undefined) {
-    throw new Error('useTheme must be used within a ThemeProvider');
-  }
-  return context;
-};
+export const useTheme = () => useContext(ThemeContext);
