6a39d19b1c5731e1a4a876491b9572c6624f36fc
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2024-11-26 12:16:24 +0000

    [WAL-7687 ] When adding a document to the call, it should be possible to provide its description

--- src/proposals/details/CallDocumentsCard.tsx
+++ src/proposals/details/CallDocumentsCard.tsx
@@ -1,34 +1,58 @@
-import { Card } from 'react-bootstrap';
+import { useEffect } from 'react';
 
+import { formatDateTime } from '@waldur/core/dateUtils';
 import { ExternalLink } from '@waldur/core/ExternalLink';
 import { translate } from '@waldur/i18n';
+import { DASH_ESCAPE_CODE } from '@waldur/table/constants';
+import Table from '@waldur/table/Table';
+import { useTable } from '@waldur/table/useTable';
 
-export const CallDocumentsCard = ({ call }) => {
+export const CallDocumentsCard = ({ call, rowActions, tableActions }) => {
+  const tableProps = useTable({
+    table: 'CallDocumentsCard',
+    fetchData: () =>
+      Promise.resolve({
+        rows: call.documents,
+        resultCount: call.documents.length,
+      }),
+  });
+
+  useEffect(() => {
+    tableProps.fetch();
+  }, [call.documents]);
+
+  const columns = [
+    {
+      title: translate('File name'),
+      render: ({ row }) => (
+        <ExternalLink
+          url={row.file}
+          label={decodeURIComponent(
+            row.file_name
+              .split('/')
+              .pop()
+              .replace(/_[^_]+\./, '.'),
+          )}
+          iconless
+        />
+      ),
+    },
+    {
+      title: translate('Description'),
+      render: ({ row }) => row.description || DASH_ESCAPE_CODE,
+    },
+    {
+      title: translate('Uploaded at'),
+      render: ({ row }) => formatDateTime(row.created),
+    },
+  ];
   return (
-    <Card className="card-bordered">
-      <Card.Header>
-        <Card.Title>
-          <h3>{translate('Documents')}</h3>
-        </Card.Title>
-      </Card.Header>
-      <Card.Body>
-        <ul>
-          {call.documents.map((document, i) => (
-            <li key={i}>
-              <ExternalLink
-                url={document.file}
-                label={decodeURIComponent(
-                  document.file_name
-                    .split('/')
-                    .pop()
-                    .replace(/_[^_]+\./, '.'),
-                )}
-                iconless
-              />
-            </li>
-          ))}
-        </ul>
-      </Card.Body>
-    </Card>
+    <Table
+      {...tableProps}
+      columns={columns}
+      verboseName={translate('Documents')}
+      tableActions={tableActions}
+      rowActions={rowActions}
+    />
   );
 };
--- src/proposals/update/documents/api.ts
+++ src/proposals/update/documents/api.ts
@@ -1,7 +1,10 @@
 import { sendForm } from '@waldur/core/api';
 
-export const attachDocuments = (call, file) =>
-  sendForm('POST', `${call.url}attach_documents/`, { documents: file });
+export const attachDocuments = (call, file, description) =>
+  sendForm('POST', `${call.url}attach_documents/`, {
+    documents: file,
+    description,
+  });
 
 export const detachDocuments = (call, file) =>
   sendForm('POST', `${call.url}detach_documents/`, { documents: file });
