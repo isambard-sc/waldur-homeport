52a1ea1ef95265a48d25dbef58113e780b9f2df7
Ilja Livenson <ilja@opennodecloud.com>
2024-12-03 19:09:36 +0000

    Expose backend_id/uuid for flavors and images

--- src/openstack/openstack-tenant/TenantImagesList.tsx
+++ src/openstack/openstack-tenant/TenantImagesList.tsx
@@ -3,6 +3,7 @@ import { FunctionComponent, useMemo } from 'react';
 import { formatFilesize } from '@waldur/core/utils';
 import { translate } from '@waldur/i18n';
 import { createFetcher } from '@waldur/table/api';
+import { DASH_ESCAPE_CODE } from '@waldur/table/constants';
 import Table from '@waldur/table/Table';
 import { useTable } from '@waldur/table/useTable';
 
@@ -29,18 +30,41 @@ export const TenantImagesList: FunctionComponent<{ resourceScope }> = ({
           title: translate('Name'),
           render: ({ row }) => row.name,
           copyField: (row) => row.name,
+          id: 'name',
+          keys: ['name'],
         },
         {
           title: translate('Minimal RAM'),
           render: ({ row }) => formatFilesize(row.min_ram),
+          id: 'min_ram',
+          keys: ['min_ram'],
         },
         {
           title: translate('Minimal disk'),
           render: ({ row }) => formatFilesize(row.min_disk),
+          id: 'min_disk',
+          keys: ['min_disk'],
+        },
+        {
+          title: translate('Backend ID'),
+          render: ({ row }) => <>{row.backend_id || DASH_ESCAPE_CODE}</>,
+          id: 'backend_id',
+          optional: true,
+          copyField: (row) => row.backend_id,
+          keys: ['backend_id'],
+        },
+        {
+          title: translate('UUID'),
+          render: ({ row }) => <>{row.uuid}</>,
+          id: 'uuid',
+          optional: true,
+          copyField: (row) => row.uuid,
+          keys: ['uuid'],
         },
       ]}
       verboseName={translate('images')}
       hasQuery={true}
+      hasOptionalColumns
     />
   );
 };
