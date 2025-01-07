79c5c0e74d484e532ad8cd421d0f34a5f87cdd54
Mark Borissov <mark.borissov@ut.ee>
2025-01-07 13:31:50 +0000

    [WAL-7864] Expose user settings in homeport

--- src/administration/marketplace/AdministrationMarketplace.tsx
+++ src/administration/marketplace/AdministrationMarketplace.tsx
@@ -1,18 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import { Table } from 'react-bootstrap';
 
 import { LoadingErred } from '@waldur/core/LoadingErred';
 import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
-import FormTable from '@waldur/form/FormTable';
 import { translate } from '@waldur/i18n';
-import { SettingsDescription } from '@waldur/SettingsDescription';
 
 import { getDBSettings } from '../settings/api';
-import { FieldRow } from '../settings/FieldRow';
-
-const MARKETPLACE_SETTINGS = SettingsDescription.find(
-  (group) => group.description === translate('Marketplace'),
-);
+import { SettingsCard } from '../settings/SettingsCard';
 
 export const AdministrationMarketplace = () => {
   const { data, error, isLoading, refetch } = useQuery(
@@ -28,18 +21,11 @@ export const AdministrationMarketplace = () => {
         loadData={refetch}
       />
     );
-  if (!data) return null;
 
-  return (
-    <FormTable.Card
-      title={MARKETPLACE_SETTINGS.description}
-      className="card-bordered mb-5"
-    >
-      <Table bordered={true} responsive={true} className="form-table">
-        {MARKETPLACE_SETTINGS.items.map((item) => (
-          <FieldRow item={item} key={item.key} value={data[item.key]} />
-        ))}
-      </Table>
-    </FormTable.Card>
-  );
+  return data ? (
+    <SettingsCard
+      groupNames={[translate('Marketplace')]}
+      settingsSource={data}
+    />
+  ) : null;
 };
--- src/administration/providers/IdentityProvidersList.tsx
+++ src/administration/providers/IdentityProvidersList.tsx
@@ -14,104 +14,137 @@ import { LoadingSpinner } from '@waldur/core/LoadingSpinner';
 import { translate } from '@waldur/i18n';
 
 import { getIdentityProviders } from '../api';
+import { getDBSettings } from '../settings/api';
+import { SettingsCard } from '../settings/SettingsCard';
 
 import { ProviderCard } from './ProviderCard';
 
 export const IdentityProvidersList = () => {
-  const { data, isLoading, error, refetch } = useQuery<
-    {},
-    {},
-    Record<string, { is_active }>
-  >(['IdentityProvidersList'], () =>
-    getIdentityProviders().then((providers) =>
-      providers.reduce(
-        (result, item) => ({ ...result, [item.provider]: item }),
-        {},
+  const {
+    data: providersData,
+    isLoading: isProvidersLoading,
+    error: providersError,
+    refetch: refetchProviders,
+  } = useQuery<{}, {}, Record<string, { is_active }>>(
+    ['IdentityProvidersList'],
+    () =>
+      getIdentityProviders().then((providers) =>
+        providers.reduce(
+          (result, item) => ({ ...result, [item.provider]: item }),
+          {},
+        ),
       ),
-    ),
   );
-  return isLoading ? (
-    <LoadingSpinner />
-  ) : error ? (
-    <LoadingErred
-      message={translate('Unable to load identity providers.')}
-      loadData={refetch}
-    />
-  ) : data ? (
-    <Card className="card-bordered">
-      <Card.Body>
-        <Row>
-          <Col xs={12} md={6} xl={4} className="mb-6">
-            <ProviderCard
-              title="Keycloak"
-              description={translate(
-                'Open source identity and access management solution.',
-              )}
-              provider={data.keycloak}
-              type={KEYCLOAK_IDP}
-              refetch={refetch}
-            />
-          </Col>
-          <Col xs={12} md={6} xl={4} className="mb-6">
-            <ProviderCard
-              title="TARA"
-              description={translate(
-                'Identity service of the Republic of Estonia',
-              )}
-              provider={data.tara}
-              type={TARA_IDP}
-              refetch={refetch}
-            />
-          </Col>
-          <Col xs={12} md={6} xl={4} className="mb-6">
-            <ProviderCard
-              title="eduTEAMS"
-              description={translate(
-                'Global federation of identity providers for researchers.',
-              )}
-              provider={data.eduteams}
-              type={EDUTEAMS_IDP}
-              refetch={refetch}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} md={6} xl={4} className="mb-6">
-            <ProviderCard
-              title={translate('Local identity provider')}
-              description={translate(
-                'Digital identity is managed by Waldur itself.',
-              )}
-              provider={{
-                is_active:
-                  ENV.plugins.WALDUR_CORE['AUTHENTICATION_METHODS'].includes(
-                    'LOCAL_SIGNIN',
-                  ),
-              }}
-              type={LOCAL_IDP}
-              refetch={refetch}
-              editable={false}
-            />
-          </Col>
-          <Col xs={12} md={6} xl={4} className="mb-6">
-            <ProviderCard
-              title="SAML2"
-              description={translate(
-                'Single sign-on using SAML2-based identity federation.',
-              )}
-              provider={{
-                is_active:
-                  ENV.plugins.WALDUR_CORE['AUTHENTICATION_METHODS'].includes(
-                    'SAML2',
-                  ),
-              }}
-              type={SAML2_IDP}
-              refetch={refetch}
-              editable={false}
-            />
-          </Col>
-        </Row>
-      </Card.Body>
-    </Card>
+  const {
+    data: settingsData,
+    isLoading: isSettingsLoading,
+    error: settingsError,
+    refetch: refetchSettings,
+  } = useQuery(['AdministrationUserSettings'], () =>
+    getDBSettings().then((response) => response.data),
+  );
+  if (isProvidersLoading || isSettingsLoading) return <LoadingSpinner />;
+  if (providersError || settingsError) {
+    let errorMessage = translate('Unable to load ');
+    if (providersError && settingsError) {
+      errorMessage += translate('providers and settings configuration');
+    } else if (providersError) {
+      errorMessage += translate('providers configuration');
+    } else {
+      errorMessage += translate('settings configuration');
+    }
+
+    return (
+      <LoadingErred
+        message={errorMessage}
+        loadData={() => {
+          refetchProviders();
+          refetchSettings();
+        }}
+      />
+    );
+  }
+  return providersData && settingsData ? (
+    <>
+      <SettingsCard
+        groupNames={[translate('User settings')]}
+        settingsSource={settingsData}
+      />
+      <Card className="card-bordered">
+        <Card.Body>
+          <Row>
+            <Col xs={12} md={6} xl={4} className="mb-6">
+              <ProviderCard
+                title="Keycloak"
+                description={translate(
+                  'Open source identity and access management solution.',
+                )}
+                provider={providersData.keycloak}
+                type={KEYCLOAK_IDP}
+                refetch={refetchProviders}
+              />
+            </Col>
+            <Col xs={12} md={6} xl={4} className="mb-6">
+              <ProviderCard
+                title="TARA"
+                description={translate(
+                  'Identity service of the Republic of Estonia',
+                )}
+                provider={providersData.tara}
+                type={TARA_IDP}
+                refetch={refetchProviders}
+              />
+            </Col>
+            <Col xs={12} md={6} xl={4} className="mb-6">
+              <ProviderCard
+                title="eduTEAMS"
+                description={translate(
+                  'Global federation of identity providers for researchers.',
+                )}
+                provider={providersData.eduteams}
+                type={EDUTEAMS_IDP}
+                refetch={refetchProviders}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6} xl={4} className="mb-6">
+              <ProviderCard
+                title={translate('Local identity provider')}
+                description={translate(
+                  'Digital identity is managed by Waldur itself.',
+                )}
+                provider={{
+                  is_active:
+                    ENV.plugins.WALDUR_CORE['AUTHENTICATION_METHODS'].includes(
+                      'LOCAL_SIGNIN',
+                    ),
+                }}
+                type={LOCAL_IDP}
+                refetch={refetchProviders}
+                editable={false}
+              />
+            </Col>
+            <Col xs={12} md={6} xl={4} className="mb-6">
+              <ProviderCard
+                title="SAML2"
+                description={translate(
+                  'Single sign-on using SAML2-based identity federation.',
+                )}
+                provider={{
+                  is_active:
+                    ENV.plugins.WALDUR_CORE['AUTHENTICATION_METHODS'].includes(
+                      'SAML2',
+                    ),
+                }}
+                type={SAML2_IDP}
+                refetch={refetchProviders}
+                editable={false}
+              />
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </>
   ) : null;
 };
