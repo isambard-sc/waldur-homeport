54ae7c051d0d2e1f6788fdbec0ba44ecb46a7503
Andrey Izman <andreyizman9@gmail.com>
2025-01-24 13:52:47 +0000

    [WAL-7939] Drop draft-js (deprecated), migrate to markdown editors

---
+++ src/form/MarkdownEditor.tsx
@@ -0,0 +1,130 @@
+import {
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  BlockTypeSelect,
+  ChangeCodeMirrorLanguage,
+  CodeToggle,
+  CreateLink,
+  InsertCodeBlock,
+  InsertTable,
+  InsertThematicBreak,
+  ListsToggle,
+  linkPlugin,
+  linkDialogPlugin,
+  tablePlugin,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  markdownShortcutPlugin,
+  ConditionalContents,
+  Separator,
+  StrikeThroughSupSubToggles,
+  diffSourcePlugin,
+  DiffSourceToggleWrapper,
+} from '@mdxeditor/editor';
+import { FC, useRef } from 'react';
+
+import { FormField } from './types';
+
+import '@mdxeditor/editor/style.css';
+import './MarkdownEditor.scss';
+
+interface MarkdownEditorProps extends FormField {
+  placeholder?: string;
+  className?: string;
+}
+
+const Toolbar = () => {
+  return (
+    <DiffSourceToggleWrapper>
+      <ConditionalContents
+        options={[
+          {
+            when: (editor) => editor?.editorType === 'codeblock',
+            contents: () => <ChangeCodeMirrorLanguage />,
+          },
+          {
+            fallback: () => (
+              <>
+                <UndoRedo />
+
+                <Separator />
+                <BoldItalicUnderlineToggles />
+                <CodeToggle />
+
+                <Separator />
+                <StrikeThroughSupSubToggles />
+
+                <Separator />
+                <ListsToggle />
+
+                <Separator />
+                <BlockTypeSelect />
+
+                <Separator />
+                <CreateLink />
+
+                <Separator />
+                <InsertTable />
+                <InsertThematicBreak />
+
+                <Separator />
+                <InsertCodeBlock />
+              </>
+            ),
+          },
+        ]}
+      />
+    </DiffSourceToggleWrapper>
+  );
+};
+
+const MarkdownEditor: FC<MarkdownEditorProps> = (props) => {
+  const initialValue = useRef(props.input?.value);
+
+  return (
+    <MDXEditor
+      className={props.className}
+      contentEditableClassName="editable-area"
+      markdown={props.input?.value || ''}
+      autoFocus={props.autoFocus}
+      placeholder={props.placeholder}
+      onChange={props.input?.onChange}
+      plugins={[
+        toolbarPlugin({ toolbarContents: () => <Toolbar /> }),
+        listsPlugin(),
+        quotePlugin(),
+        headingsPlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        tablePlugin(),
+        thematicBreakPlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: '' }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            py: 'Python',
+            java: 'Java',
+            js: 'JavaScript',
+            tsx: 'TypeScript',
+            php: 'PHP',
+            txt: 'Plain Text',
+            '': 'Unspecified',
+          },
+        }),
+        markdownShortcutPlugin(),
+        diffSourcePlugin({
+          viewMode: 'rich-text',
+          readOnlyDiff: true,
+          diffMarkdown: initialValue.current,
+        }),
+      ]}
+    />
+  );
+};
+
+export default MarkdownEditor;
