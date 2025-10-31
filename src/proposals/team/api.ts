a6e0b02a2d30f159eff951ac50b3b1dbfcbc8e80
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2025-10-31 12:52:24 +0700

    Using correct API endpoint now that it is implemented in mastermind

--- src/proposals/team/api.ts
+++ src/proposals/team/api.ts
@@ -1,15 +1,11 @@
-import { post } from '@waldur/core/api';
+import { proposalAddUserCreate } from 'waldur-js-client';
 
 /**
  * Creates a new user account or returns existing user if email already exists
  *
- * TODO: This API endpoint needs to be implemented in waldur_mastermind backend
- * The endpoint should be created at: waldur_mastermind/proposals/views.py
- *
- * Expected backend behavior:
- * - POST /api/proposals-users/
- * - Request body: { email, first_name, last_name }
- * - Response: User object (either newly created or existing)
+ * Backend endpoint: POST /api/proposal-add-user/
+ * Request body: { email, first_name, last_name }
+ * Response: User object (either newly created or existing)
  * - If user with email exists, return that user
  * - If user doesn't exist, create new user and return it
  *
@@ -27,22 +23,12 @@ export const createProposalUser = async ({
   first_name: string;
   last_name: string;
 }) => {
-  // TODO: Replace with actual API call when backend is implemented
-  // return await post('/api/proposals-users/', {
-  //   email,
-  //   first_name,
-  //   last_name,
-  // });
-
-  // Temporary mock response for development
-  return {
-    uuid: 'temp-' + Date.now(),
-    email,
-    first_name,
-    last_name,
-    full_name: `${first_name} ${last_name}`,
-    username: email.split('@')[0],
-    is_active: true,
-    registration_method: 'default',
-  };
+  const response = await proposalAddUserCreate({
+    body: {
+      email,
+      first_name,
+      last_name,
+    },
+  });
+  return response.data;
 };
