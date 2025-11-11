a3d46bed2979d248c4c5a422da96cd65af9b633a
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2025-11-11 12:17:08 +0700

    Normal users can now manage team members for a proposal

--- src/proposals/team/AddUserButton.tsx
+++ src/proposals/team/AddUserButton.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { lazyComponent } from '@waldur/core/lazyComponent';
+import { UserFeatures } from '@waldur/FeaturesEnums';
+import { isFeatureVisible } from '@waldur/features/connect';
 import { translate } from '@waldur/i18n';
 import { openModalDialog } from '@waldur/modal/actions';
 import { ActionItem } from '@waldur/resource/actions/ActionItem';
@@ -19,13 +21,23 @@ const AddUserDialog = lazyComponent(() =>
 export const AddUserButton: React.FC<AddUserDialogProps> = (props) => {
   const dispatch = useDispatch();
   const user = useSelector(getUser);
-  return (
-    user.is_staff && (
+
+  if (!user) {
+    console.log('Current user is not defined');
+    return null;
+  }
+
+  console.log('Current user:', user);
+
+  if (user.is_staff || user.is_support || isFeatureVisible(UserFeatures.allow_user_creation)) {
+    return (
       <ActionItem
         title={translate('Member')}
         action={() => dispatch(openModalDialog(AddUserDialog, props))}
         iconNode={<UserPlusIcon weight="bold" />}
       />
     )
-  );
+  } else {
+    return null;
+  }
 };
