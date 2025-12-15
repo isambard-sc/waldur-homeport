69f9158527efa445dcb380111c411f02b0920dc8
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2025-12-15 15:33:18 +0700

    User "add project role" form converted to redux form

--- src/customer/team/AddProjectUserDialog.tsx
+++ src/customer/team/AddProjectUserDialog.tsx
@@ -1,22 +1,109 @@
-import { useDispatch } from 'react-redux';
-import { reduxForm } from 'redux-form';
+import { DateTime } from 'luxon';
+import { FunctionComponent } from 'react';
+import { OptionProps, components } from 'react-select';
+import { useDispatch, useSelector } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 import { CustomerUser, Project, projectsAddUser } from 'waldur-js-client';
 
 import { SubmitButton } from '@waldur/auth/SubmitButton';
+import { required } from '@waldur/core/validators';
+import { DateField } from '@waldur/form/DateField';
 import { FormContainer } from '@waldur/form';
+import { SelectField } from '@waldur/form/SelectField';
 import { translate } from '@waldur/i18n';
+import { FormGroup } from '@waldur/marketplace/offerings/FormGroup';
 import { closeModalDialog } from '@waldur/modal/actions';
 import { CloseDialogButton } from '@waldur/modal/CloseDialogButton';
 import { ModalDialog } from '@waldur/modal/ModalDialog';
-import { Role } from '@waldur/permissions/types';
-import { ExpirationTimeGroup } from '@waldur/project/team/ExpirationTimeGroup';
-import { RoleGroup } from '@waldur/project/team/RoleGroup';
+import { Role, RoleType } from '@waldur/permissions/types';
+import { getRoles } from '@waldur/permissions/utils';
 import { showErrorResponse } from '@waldur/store/notify';
+import { getCustomer } from '@waldur/workspace/selectors';
 
-import { OrganizationProjectSelectField } from './OrganizationProjectSelectField';
 import { UserGroup } from './UserGroup';
+import { useCustomerProjects } from '../workspace/fetchCustomer';
 
 const FORM_ID = 'AddProjectUserDialog';
+
+// Redux-form compatible OrganizationProjectSelectField component
+const OrganizationProjectSelectField = ({ disabled = false }) => {
+  const currentCustomer = useSelector(getCustomer);
+  const { loading } = useCustomerProjects();
+
+  return (
+    <FormGroup label={translate('Project')} required>
+      <Field
+        name="project"
+        component={SelectField as any}
+        options={currentCustomer?.projects}
+        getOptionLabel={(option) => option.name}
+        getOptionValue={(option) => option.url}
+        isClearable={false}
+        isDisabled={disabled}
+        isLoading={loading}
+        validate={required}
+      />
+    </FormGroup>
+  );
+};
+
+// Redux-form compatible RoleGroup component
+const renderRoleType = (roleType: RoleType) =>
+  ({
+    customer: 'O',
+    project: 'P',
+    service_provider: 'SP',
+    call_organizer: 'CO',
+  })[roleType] || '';
+
+const RoleOption: FunctionComponent<OptionProps<Role>> = (props) => (
+  <components.Option {...props}>
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      {props.data.description || props.data.name}
+      <span
+        style={{
+          alignSelf: 'center',
+          marginLeft: 'auto',
+        }}
+      >
+        {renderRoleType(props.data.content_type)}
+      </span>
+    </div>
+  </components.Option>
+);
+
+const RoleGroup: FunctionComponent<{ types: RoleType[] }> = ({ types }) => (
+  <FormGroup label={translate('Role')}>
+    <Field
+      name="role"
+      component={SelectField as any}
+      options={getRoles(types)}
+      getOptionLabel={(role: Role) => role.description || role.name}
+      getOptionValue={({ name }) => name}
+      validate={required}
+      components={{ Option: RoleOption }}
+    />
+  </FormGroup>
+);
+
+// Redux-form compatible ExpirationTimeGroup component
+const ExpirationTimeGroup: FunctionComponent<{ disabled?: boolean }> = ({
+  disabled,
+}) => (
+  <FormGroup
+    id="expiration-time-group"
+    label={translate('Role expires on')}
+    spaceless
+  >
+    <Field
+      name="expiration_time"
+      component={DateField as any}
+      disabled={disabled}
+      minDate={DateTime.now().plus({ days: 1 }).toISO()}
+      placeholder="YYYY-MM-DD"
+    />
+  </FormGroup>
+);
 
 interface AddProjectUserDialogFormData {
   role: Role;
--- src/customer/team/EditProjectUserDialog.tsx
+++ src/customer/team/EditProjectUserDialog.tsx
@@ -1,6 +1,8 @@
-import { useCallback } from 'react';
+import { DateTime } from 'luxon';
+import { FunctionComponent, useCallback } from 'react';
+import { OptionProps, components } from 'react-select';
 import { connect, useDispatch } from 'react-redux';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 import {
   CustomerUser,
   NestedProjectPermission,
@@ -10,21 +12,81 @@ import {
 } from 'waldur-js-client';
 
 import { SubmitButton } from '@waldur/auth/SubmitButton';
+import { required } from '@waldur/core/validators';
+import { DateField } from '@waldur/form/DateField';
 import { FormContainer } from '@waldur/form';
+import { SelectField } from '@waldur/form/SelectField';
 import { translate } from '@waldur/i18n';
+import { FormGroup } from '@waldur/marketplace/offerings/FormGroup';
 import { closeModalDialog } from '@waldur/modal/actions';
 import { CloseDialogButton } from '@waldur/modal/CloseDialogButton';
 import { ModalDialog } from '@waldur/modal/ModalDialog';
-import { Role } from '@waldur/permissions/types';
-import { getProjectRoles } from '@waldur/permissions/utils';
-import { ExpirationTimeGroup } from '@waldur/project/team/ExpirationTimeGroup';
-import { RoleGroup } from '@waldur/project/team/RoleGroup';
+import { Role, RoleType } from '@waldur/permissions/types';
+import { getProjectRoles, getRoles } from '@waldur/permissions/utils';
 import { showErrorResponse } from '@waldur/store/notify';
 
 import { ProjectGroup } from './ProjectGroup';
 import { UserGroup } from './UserGroup';
 
 const FORM_ID = 'EditProjectUserDialog';
+
+// Redux-form compatible RoleGroup component
+const renderRoleType = (roleType: RoleType) =>
+  ({
+    customer: 'O',
+    project: 'P',
+    service_provider: 'SP',
+    call_organizer: 'CO',
+  })[roleType] || '';
+
+const RoleOption: FunctionComponent<OptionProps<Role>> = (props) => (
+  <components.Option {...props}>
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      {props.data.description || props.data.name}
+      <span
+        style={{
+          alignSelf: 'center',
+          marginLeft: 'auto',
+        }}
+      >
+        {renderRoleType(props.data.content_type)}
+      </span>
+    </div>
+  </components.Option>
+);
+
+const RoleGroup: FunctionComponent<{ types: RoleType[] }> = ({ types }) => (
+  <FormGroup label={translate('Role')}>
+    <Field
+      name="role"
+      component={SelectField as any}
+      options={getRoles(types)}
+      getOptionLabel={(role: Role) => role.description || role.name}
+      getOptionValue={({ name }) => name}
+      validate={required}
+      components={{ Option: RoleOption }}
+    />
+  </FormGroup>
+);
+
+// Redux-form compatible ExpirationTimeGroup component
+const ExpirationTimeGroup: FunctionComponent<{ disabled?: boolean }> = ({
+  disabled,
+}) => (
+  <FormGroup
+    id="expiration-time-group"
+    label={translate('Role expires on')}
+    spaceless
+  >
+    <Field
+      name="expiration_time"
+      component={DateField as any}
+      disabled={disabled}
+      minDate={DateTime.now().plus({ days: 1 }).toISO()}
+      placeholder="YYYY-MM-DD"
+    />
+  </FormGroup>
+);
 
 interface EditProjectUserDialogFormData {
   role: Role;
