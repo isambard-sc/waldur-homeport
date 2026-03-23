e895979bc5966e1093218ccd5cc909ccebad4e70
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2026-03-23 13:24:35 +0700

    Make sure to get reports from deleted / terminated projects too

--- src/openportal/reports/OrganisationReportsTab.tsx
+++ src/openportal/reports/OrganisationReportsTab.tsx
@@ -262,6 +262,7 @@ export const OrganisationReportsTab: FC = () => {
             page_size: 25,
             o: ['name'],
             page,
+            is_terminated: true,
             ...(projectSearch ? { query: projectSearch } : {}),
             ...(projectStartAfter ? { start_date_after: projectStartAfter } : {}),
             ...(projectEndBefore ? { end_date_before: projectEndBefore } : {}),
